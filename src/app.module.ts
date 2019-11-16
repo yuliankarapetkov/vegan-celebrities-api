d6a90316ad4ba414fdaf830b4c7e3eeb9061ca12
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2019-11-16 19:44:00 +0200

    typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { typeormConfig } from './config';
 import { modules } from './modules';
 
 @Module({
     imports: [
+        TypeOrmModule.forRoot(typeormConfig),
+
         ...modules
     ]
 })
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,16 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+import * as config from 'config';
+
+const dbConfig = config.get('db');
+
+export const typeormConfig: TypeOrmModuleOptions = {
+    type: dbConfig.type,
+    host: process.env.DB_HOST || dbConfig.host,
+    port: process.env.DB_PORT || dbConfig.port,
+    username: process.env.DB_USERNAME || dbConfig.username,
+    password: process.env.DB_PASSWORD || dbConfig.password,
+    database: process.env.DB_DATABASE || dbConfig.database,
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
+    synchronize: process.env.DB_SYNCHRONIZE || dbConfig.synchronize
+};
