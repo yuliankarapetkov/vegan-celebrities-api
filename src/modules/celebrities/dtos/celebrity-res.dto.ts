eb88b56b0b720733e096bec4ebc0216aa213cdaf
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2019-11-17 11:03:35 +0200

    celeb res dto fixed

--- src/modules/celebrities/dtos/celebrity-res.dto.ts
+++ src/modules/celebrities/dtos/celebrity-res.dto.ts
@@ -1,9 +1,52 @@
-import { Type, Exclude, Expose } from 'class-transformer';
+import { Exclude, Expose } from 'class-transformer';
 
 @Exclude()
 export class CelebrityResDto {
     @Expose()
     id: number;
+
     @Expose()
     name: string;
+
+    @Expose()
+    slug: string;
+
+    @Expose()
+    about: string;
+
+    @Expose()
+    category: string;
+
+    @Expose()
+    occupation: string;
+
+    @Expose()
+    imageUrl: string;
+
+    @Expose()
+    birthdate: Date;
+
+    @Expose()
+    birthplace: string;
+
+    @Expose()
+    hegiht: number;
+
+    @Expose()
+    partner: string;
+
+    @Expose()
+    wikiUrl: string;
+
+    @Expose()
+    facebookUrl: string;
+
+    @Expose()
+    instagramUrl: string;
+
+    @Expose()
+    youtubeUrl: string;
+
+    @Expose()
+    twitterUrl: string;
 }
